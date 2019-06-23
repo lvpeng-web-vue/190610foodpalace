0965f984039ed54e5771af6d9050068a61d4e0fb
lvpeng <2448918548@qq,com>
2019-06-24 04:39:27 +0800

    选择食物类别，食物滑动better-scroll,购物车控制组件完成

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,6 @@
 // 通过mutations间接更新state的多个方法的对象
-import { reqAddress, reqCategorys, reqShops, reqUser, reqLogout, reqShopGoods, reqShopInfo, reqShopRatings } from '../api/index'
-import { RECEIVE_ADDRESS, RECEIVE_CATEGORYS, RECEIVE_SHOPS, RECEIVE_USER_INFO, RESET_USER_INFO, RECEIVE_GOODS, RECEIVE_INFO, RECEIVE_RATINGS } from './mutation-types'
+import { reqAddress, reqCategorys, reqShops, reqUser, reqLogout, reqShopGoods, reqShopInfo, reqShopRatings, } from '../api/index'
+import { RECEIVE_ADDRESS, RECEIVE_CATEGORYS, RECEIVE_SHOPS, RECEIVE_USER_INFO, RESET_USER_INFO, RECEIVE_GOODS, RECEIVE_INFO, RECEIVE_RATINGS,INCREMENT_FOOD_COUNT,DECREMENT_FOOD_COUNT, } from './mutation-types'
 export default {
     // 异步获取地址
     async getAddress({ commit, state }) {
@@ -40,30 +40,43 @@ export default {
         }
     },
     // 异步获取商家信息
-    async getShopInfo({ commit }) {
+    async getShopInfo({ commit }, cb) {
         const result = await reqShopInfo()
         if (result.code === 0) {
             const info = result.data
             info.score = 3.5
             commit(RECEIVE_INFO, { info })
+            cb && cb()
         }
     },
     // 异步获取商家评价列表
-    async getShopRatings({ commit }) {
+    async getShopRatings({ commit }, cb) {
         const result = await reqShopRatings()
         if (result.code === 0) {
             const ratings = result.data
             commit(RECEIVE_RATINGS, { ratings })
+            // 数据更新后通知组件
+            // 如果cb有值 那么执行cb(),如果cb没值，那么不执行
             cb && cb()
         }
     },
     // 异步获取商家商品列表
-    async getShopGoods({ commit }) {
+    async getShopGoods({ commit }, cb) {
         const result = await reqShopGoods()
         if (result.code === 0) {
             const goods = result.data
             commit(RECEIVE_GOODS, { goods })
             // 如果组件中传递了接收消息的回调函数, 数据更新后, 调用回调通知调用的组件
+            cb && cb()
+        }
+    },
+
+    // 更新指定 food 的 count
+    updateFoodCount({ commit }, { food, isAdd }) {
+        if (isAdd) { // 增加
+            commit(INCREMENT_FOOD_COUNT, { food })
+        } else { // 减少
+            commit(DECREMENT_FOOD_COUNT, { food })
         }
     },
 }
--- src/store/muations.js
+++ src/store/muations.js
@@ -7,8 +7,11 @@ import {
     RESET_USER_INFO,
     RECEIVE_INFO,
     RECEIVE_RATINGS,
-    RECEIVE_GOODS
+    RECEIVE_GOODS,
+    INCREMENT_FOOD_COUNT,
+    DECREMENT_FOOD_COUNT
 } from "./mutation-types"
+import Vue from "vue"
 export default {
     [RECEIVE_ADDRESS](state, { address }) {
         state.address = address
@@ -27,5 +30,23 @@ export default {
     },
     [RECEIVE_INFO](state, { info }) { state.info = info },
     [RECEIVE_RATINGS](state, { ratings }) { state.ratings = ratings },
-    [RECEIVE_GOODS](state, { goods }) {state.goods = goods}
+    [RECEIVE_GOODS](state, { goods }) { state.goods = goods },
+    [INCREMENT_FOOD_COUNT](state, { food }) {
+        if (!food.count) { // 第一次增加时, 没有 count
+            // food.count = 1 // 添加 count 属性, 并指定为 1
+            // 问题: 新添加的属性没有数据劫持==>数据绑定==>更新了数据但界面不变
+            Vue.set(food, 'count', 1) // 给有数据绑定的对象添加指定属性名和值的属性(有绑定)
+            state.shopCart.push(food) // 添加到购物车
+        } else { // 有 count
+            food.count++
+        }
+    },
+    [DECREMENT_FOOD_COUNT](state, { food }) {
+        if (food.count) { // count 有值才减 1
+            food.count--
+            if (food.count === 0) {// 如果数量减为 0, 从购物车中移除
+                state.shopCart.splice(state.shopCart.indexOf(food), 1)
+            }
+        }
+    },
 }
